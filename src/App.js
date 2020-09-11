6674cc3d8b5b8b1744e928870125421ffee4e37b
Mike Lu <mikezebin@yahoo.com>
2020-09-11 23:52:09 +0800

    Added Search

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component, Fragment } from 'react';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
+import Search from './components/users/Search';
 import axios from 'axios';
 import './App.css';
 
@@ -15,7 +16,9 @@ class App extends Component {
       loading: true,
     });
 
-    const res = await axios.get('http://api.github.com/users');
+    const res = await axios.get(
+      `http://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
 
     this.setState({
       users: res.data,
@@ -28,6 +31,7 @@ class App extends Component {
       <Fragment>
         <Navbar />
         <div className='container'>
+          <Search />
           <Users loading={this.state.loading} users={this.state.users} />
         </div>
       </Fragment>
