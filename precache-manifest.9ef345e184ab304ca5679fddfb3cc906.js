ea4ca10361f9f787095fe1e1ccb91ececf5fdd41
Mike Lu <mikezebin@yahoo.com>
2020-11-11 23:01:34 +0800

    Updates

---
+++ precache-manifest.9ef345e184ab304ca5679fddfb3cc906.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ff3c986eb8034f0183e4c0007a1727d",
+    "url": "/LyricsFinder/index.html"
+  },
+  {
+    "revision": "f101dbfbb4e139b7752e",
+    "url": "/LyricsFinder/static/css/main.68e162e7.chunk.css"
+  },
+  {
+    "revision": "85e4986365e2f7bad6ea",
+    "url": "/LyricsFinder/static/js/2.6e179532.chunk.js"
+  },
+  {
+    "revision": "f101dbfbb4e139b7752e",
+    "url": "/LyricsFinder/static/js/main.2d866fbd.chunk.js"
+  },
+  {
+    "revision": "7c1eb1db8ca358548c2b",
+    "url": "/LyricsFinder/static/js/runtime~main.fca0ecc5.js"
+  }
+]);
